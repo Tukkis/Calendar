69cc985bde00a3338ddf68ae6fa7430fec917a6f
Tuukka Heinonen <tukkis96@outlook.com>
2019-11-08 09:34:29 +0200

    Fix weekday flow

--- script.js
+++ script.js
@@ -35,79 +35,86 @@ function populateCalendar(iMonth, iYear) {
     todai.setFullYear(iYear);
     todai.setMonth(iMonth);
     let filler = todai.getDay();
-    for (let i = 0; i <= filler; i++) {
-        let node = document.createElement('p');
-        days.appendChild(node);
-    }
-    for (let i = 0; i < daysI; i++) {
-        let node = document.createElement('h4');
-        let textnode = document.createTextNode(i + 1);
-        node.setAttribute("id", i+1);
-        node.addEventListener('click', addEvent);
-        node.appendChild(textnode);
-        days.appendChild(node);
-        if ((i + 1) == today.getDate() && currentMonth == today.getMonth() && currentYear == today.getFullYear()) {
-            node.classList.add('today');
+    if (filler == 0) {
+        for (let i = 0; i < 6; i++) {
+            let node = document.createElement('p');
+            days.appendChild(node);
+            }
+        } else {
+            for (let i = 2; i <= filler; i++) {
+                let node = document.createElement('p');
+                days.appendChild(node);
+            }
+        }
+        for (let i = 0; i < daysI; i++) {
+            let node = document.createElement('h4');
+            let textnode = document.createTextNode(i + 1);
+            node.setAttribute("id", i + 1);
+            node.addEventListener('click', addEvent);
+            node.appendChild(textnode);
+            days.appendChild(node);
+            if ((i + 1) == today.getDate() && currentMonth == today.getMonth() && currentYear == today.getFullYear()) {
+                node.classList.add('today');
+            }
         }
     }
-}
 
-populateCalendar(currentMonth, currentYear);
-
-function previousMonth() {
-    if (currentMonth > 0) {
-        currentMonth = currentMonth - 1;
-    }
-    else {
-        currentMonth = 11;
-        currentYear = currentYear - 1;
-    }
     populateCalendar(currentMonth, currentYear);
-    fillMonthAndYear();
-}
 
-function nextMonth() {
-    if (currentMonth < 11) {
-        currentMonth = currentMonth + 1;
-    }
-    else {
-        currentMonth = 0;
-        currentYear = currentYear + 1;
-    }
-    populateCalendar(currentMonth, currentYear);
-    fillMonthAndYear();
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    populateCalendar(currentMonth, currentYear);
-    fillMonthAndYear();
-}
-
-let lastActive = '';
-
-function addEvent() {
-    console.log(this.offsetLeft, this.offsetWidth);
-    this.classList.add('active');
-    if(this != lastActive && lastActive != ''){
-        lastActive.classList.remove('active');
+    function previousMonth() {
+        if (currentMonth > 0) {
+            currentMonth = currentMonth - 1;
         }
-    lastActive = this;
-    let node = document.createElement('form');
-    node.innerHTML =
-    `<h5>${this.id}. of ${months[currentMonth]}</h5>
+        else {
+            currentMonth = 11;
+            currentYear = currentYear - 1;
+        }
+        populateCalendar(currentMonth, currentYear);
+        fillMonthAndYear();
+    }
+
+    function nextMonth() {
+        if (currentMonth < 11) {
+            currentMonth = currentMonth + 1;
+        }
+        else {
+            currentMonth = 0;
+            currentYear = currentYear + 1;
+        }
+        populateCalendar(currentMonth, currentYear);
+        fillMonthAndYear();
+    }
+
+    function jump() {
+        currentYear = parseInt(selectYear.value);
+        currentMonth = parseInt(selectMonth.value);
+        populateCalendar(currentMonth, currentYear);
+        fillMonthAndYear();
+    }
+
+    let lastActive = '';
+
+    function addEvent() {
+        console.log(this.offsetLeft, this.offsetWidth);
+        this.classList.add('active');
+        if (this != lastActive && lastActive != '') {
+            lastActive.classList.remove('active');
+        }
+        lastActive = this;
+        let node = document.createElement('form');
+        node.innerHTML =
+            `<h5>${this.id}. of ${months[currentMonth]}</h5>
     </br>
     <input/>`;
-    node.setAttribute('id', 'eventform');
-    body.appendChild(node);
-    node.style.position = 'absolute';
-    node.style.top = (this.offsetTop + calendar.offsetTop + days.offsetTop + ((this.offsetHeight - node.offsetHeight) / 2)) + 'px';
-    node.style.left = (this.offsetLeft + calendar.offsetLeft + days.offsetLeft + ((this.offsetWidth - node.offsetWidth) / 2)) + 'px';
-    node.addEventListener('mouseleave', function(){node.remove()});
-    node.addEventListener('mouseenter', function(){node.classList.add('currentform')}); 
-}
+        node.setAttribute('id', 'eventform');
+        body.appendChild(node);
+        node.style.position = 'absolute';
+        node.style.top = (this.offsetTop + calendar.offsetTop + days.offsetTop + ((this.offsetHeight - node.offsetHeight) / 2)) + 'px';
+        node.style.left = (this.offsetLeft + calendar.offsetLeft + days.offsetLeft + ((this.offsetWidth - node.offsetWidth) / 2)) + 'px';
+        node.addEventListener('mouseleave', function () { node.remove() });
+        node.addEventListener('mouseenter', function () { node.classList.add('currentform') });
+    }
 
 
-previousButton.addEventListener('click', previousMonth);
-nextButton.addEventListener('click', nextMonth);
+    previousButton.addEventListener('click', previousMonth);
+    nextButton.addEventListener('click', nextMonth);
